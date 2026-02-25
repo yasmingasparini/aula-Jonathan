62cd0d74a957cc0c1975e315d723315135fb4f6b
yasmingasparini <yasmingl1108@gmail.com>
2026-02-25 11:10:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// console.log(taboada(5))
+
+// function taboada (a){
+//     let arrayTaboada =[]
+//     for(let i=0; i<=10; i++){
+//         arrayTaboada.push(i*a)
+//     }
+//     return arrayTaboada
+// }
+
+
+// console.log(somar(2, 3))
+// console.log(subtrair(2, 3))
+// console.log(multiplicacao(2, 3))
+// console.log(divisao(2, 3))
+// function somar(a, b){
+//     return a + b
+// }
+// function subtrair(a, b){
+//     return a - b
+// }
+// function multiplicacao(a ,b){
+//     return a * b
+// }
+// function divisao(a, b){
+//     return (a / b).toFixed(2)
+// }
+
+
+// console.log(calcular("/", 2, 3))
+// function calcular(operador, a, b){
+//     if(operador=="+"){
+//         return a + b
+//     } else if (operador == "-"){
+//         return a - b
+//     } else if (operador == "*"){
+//         return a * b
+//     } else if (operador == "/"){
+//         return a / b
+//     }
+// }
+
+
+function saudacao(nome){
+    nome = prompt ("Digite seu nome")
+    alert ("Olá" + nome)
+} 
+
+saudacao()
